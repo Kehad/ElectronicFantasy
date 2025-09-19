cef296ca14082555bdf0b1490dec302c8199c12f
KEHINDE ADIGUN <keahnney01@gmail.com>
2025-09-19 06:14:18 +0100

    new commit 7

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,8 @@
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import LandingPage from "./pages/LandingPage";
-// import SmartPowerMonitoring from "./pages/projects/SmartPowerMonitoring";
-// import IoTPowerWifiMonitor from "./pages/projects/IoTPowerWifiMonitor";
-// import FMTransmitter from "./pages/projects/FMtransmitter";
-// import CellularSignalBooster from "./pages/projects/CellularSignalBooster";
-// import SmartSecuritySystem from "./pages/projects/SmartSecuritySystem";
-// import Layout from "./pages/Layout";
-// // import HumanAssetTracker from "./pages/HumanAssetTracker";
-
-// function App() {
-//   return (
-//     <Router>
-//       <Layout>
-//         <Routes>
-//           <Route
-//             path="/projects/smart-power-monitoring"
-//             element={<SmartPowerMonitoring />}
-//           />
-//           <Route
-//             path="/projects/iot-power-wifi-monitor"
-//             element={<IoTPowerWifiMonitor />}
-//           />
-//           <Route path="/projects/fm-transmitter" element={<FMTransmitter />} />
-//           <Route path="/" element={<LandingPage />} />
-//           <Route
-//             path="/projects/cellular-signal-booster"
-//             element={<CellularSignalBooster />}
-//           />
-//           <Route
-//             path="/projects/smart-traffic-light"
-//             element={<SmartSecuritySystem />}
-//           />
-//           {/* <Route
-//             path="/projects/human-asset-tracker"
-//             element={<HumanAssetTracker />} */}
-//           {/* />  */}
-//           {/*
-          
-//           <Route
-//             path="/projects/smart-access-control"
-//             element={<SmartAccessControl />}
-//           />
-//           */}
-//           {/* <Route path="/projects/progress-report" element={<ProgressReport />} />
-//           <Route
-//             path="/projects/smart-traffic-light"
-//             element={<TrafficSystem />}
-//           />
-//           <Route
-//             path="/projects/safesphere-security"
-//             element={<SafeSphereSecurity />}
-//           /> */}
-//         </Routes>
-//       </Layout>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import SmartPowerMonitoring from "./pages/projects/SmartPowerMonitoring";
 import IoTPowerWifiMonitor from "./pages/projects/IoTPowerWifiMonitor";
-import FMTransmitter from "./pages/projects/FMTransmitter";
+import FMTransmitter from "./pages/projects/FMtransmitter";
 import CellularSignalBooster from "./pages/projects/CellularSignalBooster";
 import SmartSecuritySystem from "./pages/projects/SmartSecuritySystem";
 import SmartAccessControl from "./pages/projects/SmartAccessControl";
